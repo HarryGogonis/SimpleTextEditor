57427f48e9eacf5366b2d170f052012d17f14380
HarryGogonis <enterharry@gmail.com>
2012-06-17 00:17:00 -0400

    More features
    Added:
    +Tab system
    
    Errors:
    ! Canceling the save dialog crashes
    ! When closing program, asks if you want to save twice
    ! Need to change tab text to current file open
    Functionality Needed:
    >Save all tab files on quit

--- Source/Form1.h
+++ Source/Form1.h
@@ -43,13 +43,15 @@ namespace SimpleTextEditor {
 
 
 	private: System::Windows::Forms::SaveFileDialog^  saveFileDialog1;
-	private: System::Windows::Forms::TextBox^  textBox1;
+
 	private: System::Windows::Forms::SplitContainer^  splitContainer1;
 	private: System::Windows::Forms::MenuStrip^  menuStrip1;
 	private: System::Windows::Forms::ToolStripMenuItem^  fileToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripMenuItem^  newToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripMenuItem^  openToolStripMenuItem;
-	private: System::Windows::Forms::ToolStripMenuItem^  saveAsToolStripMenuItem;
+	public: System::Windows::Forms::ToolStripMenuItem^  saveAsToolStripMenuItem;
+	private: 
+
 
 	private: System::Windows::Forms::ToolStripMenuItem^  exitToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripMenuItem^  closeToolStripMenuItem;
@@ -65,6 +67,15 @@ namespace SimpleTextEditor {
 	private: System::Windows::Forms::ToolStripMenuItem^  copyToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripMenuItem^  cutToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripMenuItem^  pasteToolStripMenuItem;
+	public: System::Windows::Forms::TabControl^  tabControl1;
+	private: System::Windows::Forms::TabPage^  tabPage1;
+	public: 
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: 
+
+
+
+
 
 
 
@@ -97,11 +108,14 @@ namespace SimpleTextEditor {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(Form1::typeid));
 			this->openFileDialog1 = (gcnew System::Windows::Forms::OpenFileDialog());
 			this->saveFileDialog1 = (gcnew System::Windows::Forms::SaveFileDialog());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->splitContainer1 = (gcnew System::Windows::Forms::SplitContainer());
 			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
+			this->tabPage1 = (gcnew System::Windows::Forms::TabPage());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
 			this->fileToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->newToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
@@ -123,6 +137,8 @@ namespace SimpleTextEditor {
 			this->splitContainer1->Panel1->SuspendLayout();
 			this->splitContainer1->Panel2->SuspendLayout();
 			this->splitContainer1->SuspendLayout();
+			this->tabControl1->SuspendLayout();
+			this->tabPage1->SuspendLayout();
 			this->menuStrip1->SuspendLayout();
 			this->SuspendLayout();
 			// 
@@ -135,27 +151,10 @@ namespace SimpleTextEditor {
 			// 
 			this->saveFileDialog1->Filter = L"Text files (*.txt)|*.txt|All files (*.*)|*.*";
 			// 
-			// textBox1
-			// 
-			this->textBox1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(64)), static_cast<System::Int32>(static_cast<System::Byte>(64)), 
-				static_cast<System::Int32>(static_cast<System::Byte>(64)));
-			this->textBox1->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
-			this->textBox1->Dock = System::Windows::Forms::DockStyle::Fill;
-			this->textBox1->Font = (gcnew System::Drawing::Font(L"Consolas", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
-				static_cast<System::Byte>(0)));
-			this->textBox1->ForeColor = System::Drawing::Color::White;
-			this->textBox1->Location = System::Drawing::Point(0, 0);
-			this->textBox1->Margin = System::Windows::Forms::Padding(4);
-			this->textBox1->Multiline = true;
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->ScrollBars = System::Windows::Forms::ScrollBars::Vertical;
-			this->textBox1->Size = System::Drawing::Size(693, 300);
-			this->textBox1->TabIndex = 0;
-			// 
 			// splitContainer1
 			// 
 			this->splitContainer1->Dock = System::Windows::Forms::DockStyle::Fill;
-			this->splitContainer1->FixedPanel = System::Windows::Forms::FixedPanel::Panel1;
+			this->splitContainer1->FixedPanel = System::Windows::Forms::FixedPanel::Panel2;
 			this->splitContainer1->Location = System::Drawing::Point(0, 24);
 			this->splitContainer1->Name = L"splitContainer1";
 			this->splitContainer1->Orientation = System::Windows::Forms::Orientation::Horizontal;
@@ -164,10 +163,11 @@ namespace SimpleTextEditor {
 			// 
 			this->splitContainer1->Panel1->Controls->Add(this->label1);
 			this->splitContainer1->Panel1->ForeColor = System::Drawing::Color::White;
+			this->splitContainer1->Panel1Collapsed = true;
 			// 
 			// splitContainer1.Panel2
 			// 
-			this->splitContainer1->Panel2->Controls->Add(this->textBox1);
+			this->splitContainer1->Panel2->Controls->Add(this->tabControl1);
 			this->splitContainer1->Size = System::Drawing::Size(693, 341);
 			this->splitContainer1->SplitterDistance = 37;
 			this->splitContainer1->TabIndex = 4;
@@ -182,6 +182,52 @@ namespace SimpleTextEditor {
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(0, 16);
 			this->label1->TabIndex = 1;
+			// 
+			// tabControl1
+			// 
+			this->tabControl1->Appearance = System::Windows::Forms::TabAppearance::Buttons;
+			this->tabControl1->Controls->Add(this->tabPage1);
+			this->tabControl1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->tabControl1->DrawMode = System::Windows::Forms::TabDrawMode::OwnerDrawFixed;
+			this->tabControl1->Location = System::Drawing::Point(0, 0);
+			this->tabControl1->Name = L"tabControl1";
+			this->tabControl1->Padding = System::Drawing::Point(0, 0);
+			this->tabControl1->SelectedIndex = 0;
+			this->tabControl1->Size = System::Drawing::Size(693, 341);
+			this->tabControl1->TabIndex = 0;
+			this->tabControl1->Tag = L"";
+			this->tabControl1->DrawItem += gcnew System::Windows::Forms::DrawItemEventHandler(this, &Form1::tabControl1_DrawItem);
+			// 
+			// tabPage1
+			// 
+			this->tabPage1->AllowDrop = true;
+			this->tabPage1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(64)), static_cast<System::Int32>(static_cast<System::Byte>(64)), 
+				static_cast<System::Int32>(static_cast<System::Byte>(64)));
+			this->tabPage1->Controls->Add(this->textBox1);
+			this->tabPage1->Location = System::Drawing::Point(4, 30);
+			this->tabPage1->Name = L"tabPage1";
+			this->tabPage1->Size = System::Drawing::Size(685, 307);
+			this->tabPage1->TabIndex = 0;
+			this->tabPage1->Text = L"untitled";
+			// 
+			// textBox1
+			// 
+			this->textBox1->AcceptsReturn = true;
+			this->textBox1->AcceptsTab = true;
+			this->textBox1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(64)), static_cast<System::Int32>(static_cast<System::Byte>(64)), 
+				static_cast<System::Int32>(static_cast<System::Byte>(64)));
+			this->textBox1->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->textBox1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Consolas", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(0)));
+			this->textBox1->ForeColor = System::Drawing::Color::White;
+			this->textBox1->Location = System::Drawing::Point(0, 0);
+			this->textBox1->Margin = System::Windows::Forms::Padding(4);
+			this->textBox1->Multiline = true;
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->ScrollBars = System::Windows::Forms::ScrollBars::Vertical;
+			this->textBox1->Size = System::Drawing::Size(685, 307);
+			this->textBox1->TabIndex = 0;
 			// 
 			// menuStrip1
 			// 
@@ -205,44 +251,47 @@ namespace SimpleTextEditor {
 			// 
 			this->newToolStripMenuItem->Name = L"newToolStripMenuItem";
 			this->newToolStripMenuItem->ShortcutKeys = static_cast<System::Windows::Forms::Keys>((System::Windows::Forms::Keys::Control | System::Windows::Forms::Keys::N));
-			this->newToolStripMenuItem->Size = System::Drawing::Size(156, 22);
+			this->newToolStripMenuItem->Size = System::Drawing::Size(186, 22);
 			this->newToolStripMenuItem->Text = L"New";
+			this->newToolStripMenuItem->Click += gcnew System::EventHandler(this, &Form1::newToolStripMenuItem_Click);
 			// 
 			// openToolStripMenuItem
 			// 
 			this->openToolStripMenuItem->Name = L"openToolStripMenuItem";
 			this->openToolStripMenuItem->ShortcutKeys = static_cast<System::Windows::Forms::Keys>((System::Windows::Forms::Keys::Control | System::Windows::Forms::Keys::O));
-			this->openToolStripMenuItem->Size = System::Drawing::Size(156, 22);
+			this->openToolStripMenuItem->Size = System::Drawing::Size(186, 22);
 			this->openToolStripMenuItem->Text = L"Open";
 			this->openToolStripMenuItem->Click += gcnew System::EventHandler(this, &Form1::openToolStripMenuItem_Click);
 			// 
 			// saveToolStripMenuItem
 			// 
 			this->saveToolStripMenuItem->Name = L"saveToolStripMenuItem";
-			this->saveToolStripMenuItem->Size = System::Drawing::Size(156, 22);
+			this->saveToolStripMenuItem->ShortcutKeys = static_cast<System::Windows::Forms::Keys>((System::Windows::Forms::Keys::Control | System::Windows::Forms::Keys::S));
+			this->saveToolStripMenuItem->Size = System::Drawing::Size(186, 22);
 			this->saveToolStripMenuItem->Text = L"Save";
 			this->saveToolStripMenuItem->Click += gcnew System::EventHandler(this, &Form1::saveToolStripMenuItem_Click);
 			// 
 			// saveAsToolStripMenuItem
 			// 
 			this->saveAsToolStripMenuItem->Name = L"saveAsToolStripMenuItem";
-			this->saveAsToolStripMenuItem->ShortcutKeys = static_cast<System::Windows::Forms::Keys>((System::Windows::Forms::Keys::Control | System::Windows::Forms::Keys::S));
-			this->saveAsToolStripMenuItem->Size = System::Drawing::Size(156, 22);
-			this->saveAsToolStripMenuItem->Text = L"Save-As";
+			this->saveAsToolStripMenuItem->ShortcutKeys = static_cast<System::Windows::Forms::Keys>(((System::Windows::Forms::Keys::Control | System::Windows::Forms::Keys::Shift) 
+				| System::Windows::Forms::Keys::S));
+			this->saveAsToolStripMenuItem->Size = System::Drawing::Size(186, 22);
+			this->saveAsToolStripMenuItem->Text = L"Save As";
 			this->saveAsToolStripMenuItem->Click += gcnew System::EventHandler(this, &Form1::saveToolStripMenuItem_Click);
 			// 
 			// closeToolStripMenuItem
 			// 
 			this->closeToolStripMenuItem->Name = L"closeToolStripMenuItem";
 			this->closeToolStripMenuItem->ShortcutKeys = static_cast<System::Windows::Forms::Keys>((System::Windows::Forms::Keys::Control | System::Windows::Forms::Keys::W));
-			this->closeToolStripMenuItem->Size = System::Drawing::Size(156, 22);
+			this->closeToolStripMenuItem->Size = System::Drawing::Size(186, 22);
 			this->closeToolStripMenuItem->Text = L"Close";
 			this->closeToolStripMenuItem->Click += gcnew System::EventHandler(this, &Form1::closeToolStripMenuItem_Click);
 			// 
 			// exitToolStripMenuItem
 			// 
 			this->exitToolStripMenuItem->Name = L"exitToolStripMenuItem";
-			this->exitToolStripMenuItem->Size = System::Drawing::Size(156, 22);
+			this->exitToolStripMenuItem->Size = System::Drawing::Size(186, 22);
 			this->exitToolStripMenuItem->Text = L"Exit";
 			this->exitToolStripMenuItem->Click += gcnew System::EventHandler(this, &Form1::exitToolStripMenuItem_Click);
 			// 
@@ -325,6 +374,7 @@ namespace SimpleTextEditor {
 			this->Controls->Add(this->menuStrip1);
 			this->Font = (gcnew System::Drawing::Font(L"Arial", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
 				static_cast<System::Byte>(0)));
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^  >(resources->GetObject(L"$this.Icon")));
 			this->MainMenuStrip = this->menuStrip1;
 			this->Margin = System::Windows::Forms::Padding(4);
 			this->Name = L"Form1";
@@ -333,9 +383,11 @@ namespace SimpleTextEditor {
 			this->splitContainer1->Panel1->ResumeLayout(false);
 			this->splitContainer1->Panel1->PerformLayout();
 			this->splitContainer1->Panel2->ResumeLayout(false);
-			this->splitContainer1->Panel2->PerformLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->splitContainer1))->EndInit();
 			this->splitContainer1->ResumeLayout(false);
+			this->tabControl1->ResumeLayout(false);
+			this->tabPage1->ResumeLayout(false);
+			this->tabPage1->PerformLayout();
 			this->menuStrip1->ResumeLayout(false);
 			this->menuStrip1->PerformLayout();
 			this->ResumeLayout(false);
@@ -409,12 +461,13 @@ private: System::Void saveAsToolStripMenuItem_Click(System::Object^  sender, Sys
 					saveFileDialog1->ShowDialog();
 					StreamWriter ^ sw = gcnew StreamWriter(saveFileDialog1->FileName);
 					sw->WriteLine(textBox1->Text);
-					this->label1->Text = saveFileDialog1->FileName;
+					//myTabPage->Text  =  Path::GetFileName(saveFileDialog1->FileName);
 					sw->Close();
 		 }
 private: System::Void closeToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
 			AskToSave();
-			this->textBox1->Clear();
+			//this->textBox1->Clear();
+			tabControl1->TabPages->Remove(tabControl1->SelectedTab);
 		 }
 private: System::Void exitToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
 		this->Close();
@@ -424,6 +477,7 @@ private: System::Void undoToolStripMenuItem_Click(System::Object^  sender, Syste
 			 textBox1->Undo();
 		 }
 private: System::Void redoToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
+			 tabPage1->Text = Path::GetFileName(saveFileDialog1->FileName);
 
 		 }
 private: System::Void cutToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
@@ -446,7 +500,57 @@ private: System::Void aboutToolStripMenuItem_Click(System::Object^  sender, Syst
 			About ^ form = gcnew About;
 			form->ShowDialog();
 		 }
-
+private: System::Void tabControl1_DrawItem(System::Object^  sender, System::Windows::Forms::DrawItemEventArgs^  e)
+                      {
+                             Graphics^ g = e->Graphics;
+          
+                             Brush^ _TextBrush;
+                             TabPage^ _TabPage = tabControl1->TabPages[e->Index];          
+                             Rectangle _TabBounds = tabControl1->GetTabRect(e->Index);
+                    
+                             if (e->State == DrawItemState::Selected)                      
+                             {                        
+                                    _TextBrush = gcnew SolidBrush(Color::White);                         
+                                    g->FillRectangle(Brushes::DimGray, e->Bounds);
+                             }         
+                             else
+                             {
+                                    _TextBrush = gcnew SolidBrush(e->ForeColor);
+                                    e->DrawBackground();
+                             }
+             
+                             System::Drawing::Font^ _TabFont =  gcnew System::Drawing::Font("Arial", 11, FontStyle::Bold, GraphicsUnit::Pixel);
+ 
+                             StringFormat^ _StringFlags = gcnew StringFormat();                   
+                             _StringFlags->Alignment = StringAlignment::Center;
+                             _StringFlags->LineAlignment = StringAlignment::Center;        
+                             g->DrawString(_TabPage->Text, _TabFont, _TextBrush, _TabBounds, _StringFlags);               
+                      }
+private: System::Void newToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
+			String^ title = String::Concat("untitled ",
+		   (tabControl1->TabCount + 1).ToString());
+				TabPage^ myTabPage = gcnew TabPage(title);
+				TextBox^ myTextBox = gcnew TextBox();
+					myTextBox->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(64)), static_cast<System::Int32>(static_cast<System::Byte>(64)), 
+						static_cast<System::Int32>(static_cast<System::Byte>(64)));
+					myTextBox->BorderStyle = System::Windows::Forms::BorderStyle::None;
+					myTextBox->Dock = System::Windows::Forms::DockStyle::Fill;
+					myTextBox->Font = (gcnew System::Drawing::Font(L"Consolas", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+						static_cast<System::Byte>(0)));
+					myTextBox->ForeColor = System::Drawing::Color::White;
+					myTextBox->Location = System::Drawing::Point(3, 3);
+					myTextBox->Margin = System::Windows::Forms::Padding(4);
+					myTextBox->Multiline = true;
+					myTextBox->AcceptsReturn = true;
+					myTextBox->AcceptsTab = true;
+					myTextBox->Name = L"textBox1";
+					myTextBox->ScrollBars = System::Windows::Forms::ScrollBars::Vertical;
+					myTextBox->Size = System::Drawing::Size(679, 263);
+					myTextBox->TabIndex = 0;
+				tabControl1->TabPages->Add(myTabPage);
+				tabControl1->SelectedTab = myTabPage;
+				myTabPage->Controls->Add(myTextBox);
+		 }
 };
 }
 
